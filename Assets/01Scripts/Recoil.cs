a5c5662c4b5e47dc6575981412142e238374bc4e
9mean3 <gminseong114@gmail.com>
2023-05-31 19:29:01 +0900

    r

---
+++ Assets/01Scripts/Recoil.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+
+
+    [SerializeField] float recoilX;
+    [SerializeField] float recoilY;
+    [SerializeField] float recoilZ;
+
+    [SerializeField] float returnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
