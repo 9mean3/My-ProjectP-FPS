32c957ae6347b47012bb33def72a39c634aadb2a
9mean3 <gminseong114@gmail.com>
2023-06-15 20:30:58 +0900

    gg

--- Assets/01Scripts/y/Tilt.cs
+++ Assets/01Scripts/y/Tilt.cs
@@ -2,7 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum TiltState
+{
+    Idle,
+    Left,
+    Right,
+}
+
 public class Tilt : MonoBehaviour
 {
-  
+    [SerializeField] Transform target;
+
+    [SerializeField] float tiltAngleAmount;
+    [SerializeField] float tiltPositionAmount;
+
+    TiltState curTiltState;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        TiltInput();
+
+        if(curTiltState == TiltState.Idle)
+        {
+            target.localPosition = Vector3.zero;
+        }
+    }
+
+    void TiltInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if(curTiltState == TiltState.Idle || curTiltState == TiltState.Right)
+                curTiltState = TiltState.Left;
+            else
+                curTiltState = TiltState.Idle;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (curTiltState == TiltState.Idle || curTiltState == TiltState.Left)
+                curTiltState = TiltState.Right;
+            else
+                curTiltState = TiltState.Idle;
+        }
+    }
 }
